34808e87c3c68a416550fc0b2505637d4fa9639c
Niharika1203 <niharika.belle@gmail.com>
2020-08-24 21:25:29 -0700

    Initial Commit

---
+++ Tip-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Niharika Srivastav on 8/24/20.
+//  Copyright © 2020 Niharika Srivastav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
